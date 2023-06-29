482c9906521371a141e45bfeb75508339dc80a3b
ChristopherVuvu <vuvumumo93@gmail.com>
2023-06-30 00:03:29 +0300

    added

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ApplicationRecord
+    validates :name, presence: true
+    validates :emails, uniqueness: true
+end
+
+class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category {in:["Fiction", "Non-Fiction"]}
+    validates :content, lengtg:{minimum: 100}
 end
